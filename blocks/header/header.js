b33f9f940067bb498c0eacee8a8d61bc676d91d1
Tero Pikala <tero@pikala.com>
2025-11-13 13:28:25 +0000

    Linting

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -15,13 +15,11 @@ function createLogo() {
   return a;
 }
 
-
 /**
  * loads and decorates the header, mainly the nav
  * @param {Element} block The header block element
  */
 export default async function decorate(block) {
-
   // load nav as fragment
   const navMeta = getMetadata('nav');
   const navPath = navMeta ? new URL(navMeta, window.location).pathname : '/nav';
